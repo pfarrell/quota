5083afdf901f92be6608cbc634f13876c2d47f17
Pat Farrell <mr.patfarrell@gmail.com>
2014-12-02 22:43:11 -0800

    rudimentary author mod

--- routes/authors.rb
+++ routes/authors.rb
@@ -11,11 +11,16 @@ class Quota < Sinatra::Application
   get 'admin/authors' do
     haml :'admin/author'
   end
+
+  get '/admin/author/:id' do
+    haml :'admin/author', locals: {a: Author[params[:id]]}
+  end
   
-  post 'authors/new' do
-    author = Author.new
+  post '/admin/author/:id' do
+    author = Author[params[:id]]
     author.name = params[:name]
-    author.image = params[:image]
-    author.save!
+    author.image_url = params[:image_url]
+    author.save
+    redirect(url_for('/'))
   end
 end
