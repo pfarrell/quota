e6cda6f336159dd9c3a68407c7fcde100db42054
Patrick Farrell <mr.patfarrell@gmail.com>
2021-09-22 22:24:06 -0400

    add images as shared dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :deploy_via, :remote_cache
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{log tmp}
+set :linked_dirs, %w{log tmp images}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
