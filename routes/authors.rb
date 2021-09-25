6c873865fe8c2fa990ba1e86c9fe41d4836d1203
Patrick Farrell <mr.patfarrell@gmail.com>
2021-09-24 23:02:32 -0400

    fix Alben Barkley quote and author admin

--- routes/authors.rb
+++ routes/authors.rb
@@ -1,21 +1,13 @@
 class Quota < Sinatra::Application
 
   get '/admin/authors' do
-    quote = Quote[rand(1..Quote.count)]
-    respond_to do |wants|
-      wants.html { haml :index, locals: {quote: quote} } 
-      wants.json { quote.to_json }
-    end
-  end
-
-  get 'admin/authors' do
-    haml :'admin/author'
+    haml :'admin/author', locals: {a: nil}
   end
 
   get '/admin/author/:id' do
     haml :'admin/author', locals: {a: Author[params[:id]]}
   end
-  
+
   post '/admin/author/:id' do
     author = Author[params[:id]]
     author.name = params[:name]
